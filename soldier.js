a45f9c1993a034d64a52a08bb93d2bc4bd54a9a7
Duzhiev <magomed.duzhiev@gmail.com>
2022-11-02 11:31:40 +0300

    Добавил методы

--- soldier.js
+++ soldier.js
@@ -1,9 +1,37 @@
 const soldier = {
     soldierName: 'Price',
-    health: 100,
+    health: 10,
     weapon: {
        gun: 'Ak-47',
-       cartridges: 30 
+       cartridges: 30, 
     },
     supplies: 3,
-}
+   
+    getStatus (){
+        return `Имя: ${this.soldierName}, Здоровье: ${this.health}, Оружие: ${this.weapon.gun}, Кол-во патронов: ${this.weapon.cartridges}, Боеприпасы: ${this.supplies}`
+    },
+
+    shoot () {
+        this.weapon.cartridges --
+        console.log(`Бах-Бах ${this.weapon.cartridges}/30`)
+        if(this.weapon.cartridges === 0) {
+            return `Обойма пуста. Перезарядитесь! ${this.weapon.cartridges}/30`
+        }
+    },
+    recharge () {
+        this.weapon.cartridges = 30;
+        this.supplies --;
+        console.log('Перезарядка...')
+        if (this.supplies === 0) {
+            console.log('Не осталось припасов')
+        }
+    },
+    toHart() {
+        this.health --;
+        if(this.health === 0){
+            console.log('Ты умер, СЛАБАК!')
+        }
+    },
+    
+};
+console.log(soldier.getStatus())
